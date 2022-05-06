b4266d323a01344980782238f81629de9540a9cf
atalman <atalman@fb.com>
2022-05-06 06:08:21 -0700

    Cleanup remove deprecated cuda version

--- conda/build_pytorch.sh
+++ conda/build_pytorch.sh
@@ -270,9 +270,6 @@ else
     if [[ "$desired_cuda" == "11.6" ]]; then
         export CONDA_CUDATOOLKIT_CONSTRAINT="    - cudatoolkit >=11.6,<11.7 # [not osx]"
         export MAGMA_PACKAGE="    - magma-cuda116 # [not osx and not win]"
-    elif [[ "$desired_cuda" == "11.5" ]]; then
-        export CONDA_CUDATOOLKIT_CONSTRAINT="    - cudatoolkit >=11.5,<11.6 # [not osx]"
-        export MAGMA_PACKAGE="    - magma-cuda115 # [not osx and not win]"
     elif [[ "$desired_cuda" == "11.3" ]]; then
         export CONDA_CUDATOOLKIT_CONSTRAINT="    - cudatoolkit >=11.3,<11.4 # [not osx]"
         export MAGMA_PACKAGE="    - magma-cuda113 # [not osx and not win]"
